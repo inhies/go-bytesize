165142203bf9d354de608d42666cf8ea03786a78
inhies <inhies@gmail.com>
2014-01-16 05:02:04 -1000

    Add math example

--- example_test.go
+++ example_test.go
@@ -13,6 +13,16 @@ func ExampleNew() {
 	// 1.00KB
 }
 
+func ExampleNew_math() {
+	b1 := bytesize.New(1024)
+	b2 := bytesize.New(4096)
+	sum := b1 + b2
+	fmt.Printf("%s", total)
+
+	// Output:
+	// 5.00KB
+}
+
 func ExampleParse() {
 	b, _ := bytesize.Parse("1024 GB")
 	fmt.Printf("%s", b)
